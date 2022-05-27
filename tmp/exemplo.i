255e586c8b82b122638d8ea90cbc7496c5d7f514
EduardoBR03 <e01475369@gmail.com>
2022-05-27 15:22:09 -0500

    first commit

---
+++ tmp/exemplo.i
@@ -0,0 +1,4 @@
+L1:	iffalse a goto L2
+L3:	a = a + i
+	goto L1
+L2:
---
+++ tmp/exemplo.java
@@ -0,0 +1,26 @@
+package tmp;
+class Exemplo{
+public static void main(String[] args) {
+boolean stop=false;
+while(!stop){
+switch(gotoL){
+case 0:
+case 1:
+if ( !(a) ) {
+ gotoL = 2;
+ break;
+ }
+case 3:
+a=a+i;
+case -1:
+ gotoL = 1;
+ break;
+case 2:
+stop = true;
+}
+}
+}
+public static int gotoL=1;
+public static boolean a;
+public static boolean i;
+}
